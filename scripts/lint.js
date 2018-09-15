ff8a8ac4b95a1e97c153a96210f261f58867af79
Danilo Cestari <danilofcestari@gmail.com>
2018-09-16 00:15:44 +0200

    Install eslint-config-endouble and adapt existing code

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
@@ -7,7 +5,7 @@ process.env.NODE_ENV = 'development';
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
     throw err;
 });
 
@@ -16,7 +14,8 @@ require('../config/env');
 
 const paths = require('../config/paths');
 
-const CLIEngine = require("eslint").CLIEngine;
+const { CLIEngine } = require('eslint');
+
 const cli = new CLIEngine({
     useEslintrc: true,
 });
--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const App = () => (
+    <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
-  }
-}
+    </div>
+);
 
 export default App;
